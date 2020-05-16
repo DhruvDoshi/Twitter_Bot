c791ae784d637ce96f2779133c206262ed862298
DhruvDoshi <dhruvdoshi25071999@gmail.com>
2020-05-16 21:04:49 +0530

    Added tweeting function

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ var secret = {
 }
 
 var Twitter = new TwitterPackage(secret)
+
+
+var hashtag = '#covid' //puting covid as it is coronavirus lockdown period
+tweet ('This Covid-19 will teach us great things', null, null);
+//Adding the tweeting function 
+function tweet(statusMsg, screen_name, status_id){
+    Console.log('Sending tweeet to ' + screen_name)
+    Console.log('In resonse to ' + status_id)
+    var msg = statusMsg;
+    if (screen_name != null){
+        msg = '@' + screen_name + ' ' + status_id;
+    }
+    console.log('Tweet:' + msg);
+    Twitter.post('statuses/update', {
+        status: msg
+    },function(err, resonse){
+        //suppose there comes an error while tweeting the tweet
+        if (err){
+            console.log('Something went wrong while TWEETING from the bot...');
+            console.log(err);
+        }
+        else if (resonse){
+            console.log('TWEETED SUCESSFULLY !!')
+            console.log(resonse)
+        }
+    });
+}
