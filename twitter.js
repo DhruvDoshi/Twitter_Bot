9fb1c3ec509ba4175ab0e695ea9de78d4426c9c2
DhruvDoshi <dhruvdoshi25071999@gmail.com>
2020-05-16 21:23:26 +0530

    adding  top news function and connection with api

---
+++ twitter.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+var Twitter = new TwitterPackage(secret)
+
+topNewsTwitter('cnn', null);
+function topNewsTwitter(newSource, screen_name, status_id) {
+    request({
+        url: 'https://newsapi.org/v1/articles?source=' + newSource + '@apikey= MYAPIKEY', method: 'GET'
+    },
+    function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var botResponse = JSON.parse(body);
+            console.log(botResponse);
+            tweetTopArticle(botResponse.articles, screen_name);
+        } 
+        else{
+            console.log('Sorry Noting is Cooking in the World!!');
+        }
+    });
+}
+
+
